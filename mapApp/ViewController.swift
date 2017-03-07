703760432a2a45c86ec600ef2fb261093e26499b
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2017-03-07 23:53:15 +0900

    add map

--- mapApp/ViewController.swift
+++ mapApp/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 中心座標 日本
+        let center = CLLocationCoordinate2DMake(35.690553, 139.699579)
+        
+        // 表示範囲
+        let span = MKCoordinateSpanMake(0.001, 0.001)
+        
+        // 中心座標と表示範囲をマップに登録する
+        MKCoordinateRegionMake(center, span)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
