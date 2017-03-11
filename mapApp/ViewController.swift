c8146d8c9b522f34a14dfab04ec53cccd2dc185b
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2017-03-11 11:30:44 +0900

    位置情報の取得

--- mapApp/ViewController.swift
+++ mapApp/ViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var mapView: MKMapView!
-
     @IBOutlet var searchBar: UISearchBar!
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
 
         // アプリを開いたら新宿駅が中心になるように設定
         let center = CLLocationCoordinate2DMake(35.690553, 139.699579)
@@ -30,6 +33,44 @@ class ViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
         
         mapView.delegate = self
         searchBar.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard CLLocationManager.locationServicesEnabled() == true else {
+            alertMessage(message: "位置情報サービスがONになっていないため利用できません。「設定」⇒「プライバシー」⇒「位置情報サービス」")
+            return;
+        }
+        
+        switch CLLocationManager.authorizationStatus() {
+        
+        // 未設定の場合
+        case CLAuthorizationStatus.notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        
+        // 機能制限されている場合
+        case CLAuthorizationStatus.restricted:
+            alertMessage(message: "位置情報サービスの利用が制限されている利用できません。「設定」⇒「一般」⇒「機能制限」")
+            
+        // 許可しないに設定されている場合
+        case CLAuthorizationStatus.denied:
+            alertMessage(message: "位置情報の利用が許可されていないため利用できません。「設定」⇒「プライバシー」⇒「位置情報サービス」⇒「アプリ名」")
+        
+        // このAppの使用中のみ許可に設定されている場合
+        case CLAuthorizationStatus.authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            
+        // 常に許可に設定されている場合
+        case CLAuthorizationStatus.authorizedAlways:
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func alertMessage(message: String) {
+        let alertController = UIAlertController(title: "注意", message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func pressMap(_ sender: UILongPressGestureRecognizer) {
@@ -89,6 +130,11 @@ class ViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
     @IBAction func tapScreen(_ sender: Any) {
         self.view.endEditing(true)
     }
+    
+    // 位置情報を取得する
+    // 現在地を表示できる、グーグルのAPIを使用して広範囲の検索ができる
+    // http://hajihaji-lemon.com/smartphone/swift/cllocationmanager/
+    // http://hajihaji-lemon.com/smartphone/swift/cllocationmanager_pin/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
